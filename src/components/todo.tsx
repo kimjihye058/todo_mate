0dbbe304288329330eb37c9c4a7c7762bf95b879
kimjihye058 <s2321@e-mirim.hs.kr>
2025-09-08 16:34:05 +0900

    #2 todo add 스타일링

--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -3,27 +3,38 @@ import styled from "styled-components";
 function Todo() {
   return (
     <div>
-      <Category>
-        <GlobalIcon></GlobalIcon>
-        <CategoryTitle>할 일</CategoryTitle>
-        <Plus></Plus>
-      </Category>
+      <CategoryBtn>
+        <GlobalIcon />
+        <CategoryTitle>todo</CategoryTitle>
+        <Plus />
+      </CategoryBtn>
+      <TodoList>
+        <CheckBtn>
+          <CheckBtnImg></CheckBtnImg>
+        </CheckBtn>
+        <InputBox type="text" placeholder="할 일 입력" />
+        <OptionBtn>
+          <OptionBtnImg></OptionBtnImg>
+        </OptionBtn>
+      </TodoList>
     </div>
   );
 }
+
 export default Todo;
 
-export const Category = styled.button`
+export const CategoryBtn = styled.button`
   border-radius: 50px;
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #F2F2F2;
+  background-color: #f2f2f2;
   padding: 8px 10px;
+  margin-top: 8px;
 `;
 
 export const GlobalIcon = styled.img.attrs({
-  src: "./images/feed/goalIcon.svg",
+  src: "./images/feed/globalIcon.svg",
 })`
   width: 16px;
   height: 16px;
@@ -43,4 +54,50 @@ export const Plus = styled.img.attrs({
   width: 20px;
   height: 20px;
   margin-left: 5px;
+`;
+
+export const TodoList = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const CheckBtn = styled.button`
+  background-color: white;
+  padding: 0;
+  height: 21px;
+  margin-right: 10px;
+`;
+
+export const CheckBtnImg = styled.img.attrs({
+  src: "./images/feed/goalIcon.svg",
+})`
+  width: 21px;
+  height: 21px;
+`;
+
+export const InputBox = styled.input`
+  border: none;
+  border-bottom: 2px solid #000;
+  width: 377px;
+  height: 40px;
+  ::placeholder {
+    font-family: Pretendard;
+    font-weight: 500;
+    font-size: 15px;
+    color: #4c4c4c;
+  }
+`;
+
+export const OptionBtn = styled.button`
+  background-color: white;
+  padding: 0;
+  height: 20px;
+`;
+
+export const OptionBtnImg = styled.img.attrs({
+  src: "./images/option/feedOptionMenuIcon.png",
+})`
+  width: 20px;
+  height: 20px;
 `;
