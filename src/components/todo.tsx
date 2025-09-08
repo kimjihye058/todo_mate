8be3b192724f07335e5ac78006d97b15eb586a22
kimjihye058 <s2321@e-mirim.hs.kr>
2025-09-08 15:01:57 +0900

    #2 todo 등록 버튼 스타일링

--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -1,8 +1,46 @@
+import styled from "styled-components";
+
 function Todo() {
   return (
     <div>
-      <div></div>
+      <Category>
+        <GlobalIcon></GlobalIcon>
+        <CategoryTitle>할 일</CategoryTitle>
+        <Plus></Plus>
+      </Category>
     </div>
   );
 }
 export default Todo;
+
+export const Category = styled.button`
+  border-radius: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #F2F2F2;
+  padding: 8px 10px;
+`;
+
+export const GlobalIcon = styled.img.attrs({
+  src: "./images/feed/goalIcon.svg",
+})`
+  width: 16px;
+  height: 16px;
+  margin-right: 5px;
+`;
+
+export const CategoryTitle = styled.p`
+  font-family: Pretendard;
+  font-weight: 600;
+  font-size: 14px;
+  margin: 0;
+`;
+
+export const Plus = styled.img.attrs({
+  src: "./images/feed/feedAddBtnIcon.png",
+})`
+  width: 20px;
+  height: 20px;
+  margin-left: 5px;
+`;
