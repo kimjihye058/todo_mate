5991a9903367de9c541bbaddcb40d97b8db3f00c
kimjihye058 <s2321@e-mirim.hs.kr>
2025-09-09 11:14:15 +0900

    #2 todo emotion css 활용

--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -1,103 +1,190 @@
-import styled from "styled-components";
+import { atom, useAtom } from "jotai";
+import { useState } from "react";
+import { css } from "@emotion/react";
+
+const showTodoListAtom = atom(false);
+const todosAtom = atom<string[]>([]);
 
 function Todo() {
+  const [showTodoList, setShowTodoList] = useAtom(showTodoListAtom);
+  const [todos, setTodos] = useAtom(todosAtom);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleCategoryClick = () => {
+    setShowTodoList((prev) => !prev);
+  };
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.nativeEvent.isComposing) return;
+
+    if (e.key === "Enter" && inputValue.trim() !== "") {
+      setTodos([...todos, inputValue.trim()]);
+      setInputValue("");
+    }
+  };
+
   return (
     <div>
-      <CategoryBtn>
-        <GlobalIcon />
-        <CategoryTitle>todo</CategoryTitle>
-        <Plus />
-      </CategoryBtn>
-      <TodoList>
-        <CheckBtn>
-          <CheckBtnImg></CheckBtnImg>
-        </CheckBtn>
-        <InputBox type="text" placeholder="할 일 입력" />
-        <OptionBtn>
-          <OptionBtnImg></OptionBtnImg>
-        </OptionBtn>
-      </TodoList>
+      <button
+        css={css`
+          border-radius: 50px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          background-color: #f2f2f2;
+          padding: 8px 10px;
+          margin-top: 8px;
+        `}
+        onClick={handleCategoryClick}
+      >
+        <img
+          src="./images/feed/globalIcon.svg"
+          css={css`
+            width: 16px;
+            height: 16px;
+            margin-right: 5px;
+          `}
+        />
+        <p
+          css={css`
+            font-family: Pretendard;
+            font-weight: 600;
+            font-size: 14px;
+            margin: 0;
+          `}
+        >
+          todo
+        </p>
+        <img
+          src="./images/feed/feedAddBtnIcon.png"
+          css={css`
+            width: 20px;
+            height: 20px;
+            margin-left: 5px;
+          `}
+        />
+      </button>
+
+      {showTodoList && (
+        <div
+          css={css`
+            gap: 12px;
+          `}
+        >
+          <div
+            css={css`
+              display: flex;
+              justify-content: center;
+              align-items: center;
+            `}
+          >
+            <button
+              css={css`
+                background-color: white;
+                padding: 0;
+                height: 21px;
+                margin-right: 10px;
+              `}
+            >
+              <img
+                src="./images/feed/goalIcon.svg"
+                css={css`
+                  width: 21px;
+                  height: 21px;
+                `}
+              />
+            </button>
+            <input
+              type="text"
+              placeholder="할 일 입력"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+              onKeyDown={handleKeyDown}
+              css={css`
+                border: none;
+                border-bottom: 2px solid #000;
+                width: 377px;
+                height: 40px;
+                ::placeholder {
+                  font-family: Pretendard;
+                  font-weight: 500;
+                  font-size: 15px;
+                  color: #4c4c4c;
+                }
+              `}
+            />
+            <button
+              css={css`
+                background-color: white;
+                padding: 0;
+                height: 20px;
+              `}
+            >
+              <img
+                src="./images/option/feedOptionMenuIcon.png"
+                css={css`
+                  width: 20px;
+                  height: 20px;
+                `}
+              />
+            </button>
+          </div>
+
+          {todos.map((todo, index) => (
+            <div
+              key={index}
+              css={css`
+                display: flex;
+                justify-content: center;
+                align-items: center;
+              `}
+            >
+              <button
+                css={css`
+                  background-color: white;
+                  padding: 0;
+                  height: 21px;
+                  margin-right: 10px;
+                `}
+              >
+                <img
+                  src="./images/feed/goalIcon.svg"
+                  css={css`
+                    width: 21px;
+                    height: 21px;
+                  `}
+                />
+              </button>
+              <p
+                css={css`
+                  font-family: Pretendard;
+                  font-weight: 600;
+                  font-size: 14px;
+                  margin: 0;
+                `}
+              >
+                {todo}
+              </p>
+              <button
+                css={css`
+                  background-color: white;
+                  padding: 0;
+                  height: 20px;
+                `}
+              >
+                <img
+                  src="./images/option/feedOptionMenuIcon.png"
+                  css={css`
+                    width: 20px;
+                    height: 20px;
+                  `}
+                />
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
 
 export default Todo;
-
-export const CategoryBtn = styled.button`
-  border-radius: 50px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #f2f2f2;
-  padding: 8px 10px;
-  margin-top: 8px;
-`;
-
-export const GlobalIcon = styled.img.attrs({
-  src: "./images/feed/globalIcon.svg",
-})`
-  width: 16px;
-  height: 16px;
-  margin-right: 5px;
-`;
-
-export const CategoryTitle = styled.p`
-  font-family: Pretendard;
-  font-weight: 600;
-  font-size: 14px;
-  margin: 0;
-`;
-
-export const Plus = styled.img.attrs({
-  src: "./images/feed/feedAddBtnIcon.png",
-})`
-  width: 20px;
-  height: 20px;
-  margin-left: 5px;
-`;
-
-export const TodoList = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const CheckBtn = styled.button`
-  background-color: white;
-  padding: 0;
-  height: 21px;
-  margin-right: 10px;
-`;
-
-export const CheckBtnImg = styled.img.attrs({
-  src: "./images/feed/goalIcon.svg",
-})`
-  width: 21px;
-  height: 21px;
-`;
-
-export const InputBox = styled.input`
-  border: none;
-  border-bottom: 2px solid #000;
-  width: 377px;
-  height: 40px;
-  ::placeholder {
-    font-family: Pretendard;
-    font-weight: 500;
-    font-size: 15px;
-    color: #4c4c4c;
-  }
-`;
-
-export const OptionBtn = styled.button`
-  background-color: white;
-  padding: 0;
-  height: 20px;
-`;
-
-export const OptionBtnImg = styled.img.attrs({
-  src: "./images/option/feedOptionMenuIcon.png",
-})`
-  width: 20px;
-  height: 20px;
-`;
