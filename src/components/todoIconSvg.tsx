cf09783d93a247d24757f2d257a64e52e879152e
kimjihye058 <s2321@e-mirim.hs.kr>
2025-09-11 16:28:57 +0900

    #11 todoIconSvg 스타일 변경

--- src/components/todoIconSvg.tsx
+++ src/components/todoIconSvg.tsx
@@ -10,10 +10,10 @@ const TodoIconSvg = ({ colors }: TodoIconSvgProps) => {
       fill = [colors[0], colors[0], colors[0], colors[0]];
       break;
     case 2:
-      fill = [colors[0], colors[1], colors[1], colors[0]];
+      fill = [colors[1], colors[0], colors[1], colors[0]];
       break;
     case 3:
-      fill = [colors[0], colors[0], colors[2], colors[1]];
+      fill = [colors[2], colors[1], colors[0], colors[0]];
       break;
     case 4:
       fill = [colors[1], colors[3], colors[2], colors[0]];
@@ -34,28 +34,28 @@ const TodoIconSvg = ({ colors }: TodoIconSvgProps) => {
         cx="6.46154"
         cy="14.5387"
         r="6.46154"
-        fill={fill[1]}
+        fill={fill[2]}
         fillOpacity={colors.length > 1 ? "0.9" : "1"}
       />
       <circle
         cx="14.5387"
         cy="14.5387"
         r="6.46154"
-        fill={fill[2]}
+        fill={fill[0]}
         fillOpacity={colors.length > 1 ? "0.9" : "1"}
       />
       <circle
         cx="14.5387"
         cy="6.46154"
         r="6.46154"
-        fill={fill[3]}
+        fill={fill[1]}
         fillOpacity={colors.length > 1 ? "0.9" : "1"}
       />
       <circle
         cx="6.46154"
         cy="6.46154"
         r="6.46154"
-        fill={fill[0]}
+        fill={fill[3]}
         fillOpacity={colors.length > 1 ? "0.9" : "1"}
       />
     </svg>
